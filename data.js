d4b87bc0c2472c2ce308d4d44c531d46cf97b633
Filip Danisko <filip@danisko.cz>
2015-11-26 18:24:22 +0100

    Invoice rewritten

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var shopify = {
   shop: {
     name: "Shopify-invoice-test.cz",
     email: "info@Shopify-invoice-test.cz",
-    domain: "www.shopify-invoice-test.cz.cz",
+    domain: "www.shopify-invoice-test.cz",
     address: "Dlouhá 22",
     city: "Brno",
     zip: "745 45",
@@ -17,30 +17,27 @@ var shopify = {
     city: "Ostrava",
     country: "Czech Republic"
   },
+  shipping_address: true,
   order_number: 10001,
   gateway: "Platba Převodem",
   transaction: {
     date: function(){ return new Date().toJSON().slice(0,10) }
   },
+  shipping_price: 2000,
   line_items: {
     0: {
       title: "Toustovač",
-      quantity: 1,
-      price: 1500,
-    },
-    1: {
-      title: "Roh jednorožce",
       quantity: 2,
-      price: 5000,
+      price: 10000,
     }
   },
   discounts: {
     0: {
       code: 123456,
-      savings: 150
+      savings: 50
     }
   },
-  total_price: 6350
+  total_price: 17000
 };
 
 
--- dev/script.js
+++ dev/script.js
@@ -1,32 +1,119 @@
 var ShopifyCustomInvoice = (function(){
-'use strict';
- var defaults = {
-  dph: 21,
-  mena: "Kč"
- };
+ 'use strict';
 
- var calc = {
-  produktBezDph: function(price) { return price + " " + defaults['mena']; },
-  produktPouzeDph: function(price) {return Math.round( (price * defaults['dph'] / 100 ))},
-  produktCelkem: function(price, quantity) { return Math.round( (price * defaults['dph'] / 100 ) + price )  + " " + defaults['mena'] },
-}
+  var params = {
+  },
+  mena = '',
 
- var init = function() {
-  calculateProducts();
- }
 
- var calculateProducts = function() {
-  [].forEach.call(document.querySelectorAll('.produkt'), function(el,i,a) {
-    var price = el.getAttribute('data-price'),
-        quantity = el.getAttribute('data-quantity');
+  init = function(paramObject) {
 
-    el.querySelector('.produktBezDph').innerText = calc.produktBezDph(price);
-    el.querySelector('.produktDph').innerText = defaults['dph'] + " %";
-    el.querySelector('.produktPouzeDph').innerText = calc.produktPouzeDph(price);
-    el.querySelector('.produktCelkem').innerText = calc.produktCelkem(price,quantity);
+    var defaults = {
+      dph:  0.21,
+      dphzceny: 17.355,
+      mena: "Kč",
+    };
 
-  });
- };
 
- init();
+    //Set Parameters
+    var finalParams = defaults;
+    // We iterate over each property of the paramObject
+    for (var key in paramObject) {
+      // If the current property wasn't inherited, proceed
+      if (paramObject.hasOwnProperty(key)) {
+        // If the current property is defined,
+        // add it to finalParams
+        if (paramObject[key] !== undefined) {
+            finalParams[key] = paramObject[key];
+        }
+      }
+    }
+
+    params = finalParams;
+
+    // Set Mena
+    mena =  " " + params['mena'],
+
+
+    console.log(finalParams);
+
+    calculateProducts();
+    calculateInvoice();
+  },
+
+  calculateProducts = function() {
+    [].forEach.call(document.querySelectorAll('.produkt'), function(el,i,a) {
+      var
+      price = Number(el.getAttribute('data-price') / 100),
+      quantity = Number(el.getAttribute('data-quantity'));
+
+      var cenaZaKus = price;
+      var dph = ((price * quantity * params['dphzceny']) / 100).toFixed(2);
+      var celkemBezDph = (cenaZaKus * quantity) - dph;
+      var celkemSDph = (price * quantity).toFixed(2);
+
+      el.querySelector('.produkt-cena-za-kus').innerText = cenaZaKus + mena;
+      el.querySelector('.produktDph').innerText = params['dph'] * 100 + " %";
+      el.querySelector('.produkt-celkem-dph').innerText = dph + mena;
+
+      var produktCelkem = el.querySelector('.produkt-celkem-s-dph');
+      produktCelkem.innerText = celkemSDph + mena;
+      produktCelkem.setAttribute('data-price', celkemSDph);
+    });
+  },
+
+  calculateInvoice = function() {
+    var invoice = {
+      celkemBezDph: 0,
+      mezisoucet: 0,
+      dan: 0,
+      halere: 0,
+      sleva: 0,
+      celkem: 0,
+      doprava: 0,
+    };
+
+    //Get celkem
+    invoice['celkem'] = document.querySelector('.invoiceCelkem').getAttribute('data-cc') / 100;
+
+    //halere
+    var celkovaCenaZaokrouhlena = Math.round(invoice['celkem']);
+    invoice['halere'] = Math.round( (celkovaCenaZaokrouhlena - invoice['celkem']) * 100) / 100;
+
+    //Sleva
+    var slevaEl = document.querySelector('.discount-price-with-decimals') || null;
+    if (slevaEl) {invoice['sleva'] = parseFloat(slevaEl.getAttribute('data-sale'));};
+
+    //doprava
+    invoice['doprava'] = document.querySelector('.shipping-price-with-decimals').getAttribute('data-shipping') / 100;
+
+    //celekme s dani
+    var celkemSDph = invoice['celkem'] + invoice['sleva']  - invoice['doprava'];
+
+    //dan
+    invoice['dan'] = Math.round(celkemSDph * params['dphzceny']) / 100;
+
+    //celkem bez dane
+    invoice['celkemBezDph'] = celkemSDph - invoice['dan'];
+
+    //Display
+    document.querySelector('.invoiceCelkemBezDph').innerText = invoice['celkemBezDph'] + mena;
+    document.querySelector('.invoiceDph').innerText = invoice['dan'] + mena;
+    document.querySelector('.invoiceHalere').innerText = invoice['halere'] + mena;
+    document.querySelector('.invoiceCelkem').innerText = invoice['celkem'] + mena;
+  };
+
+  return init;
 })();
+
+
+
+/////////////////////////////
+// Inicializce vypoctu faktury
+//
+ShopifyCustomInvoice({
+  dph: 0.21,
+  dphzceny: 17.355,
+  mena: "Kč"
+});
+////////////////////
